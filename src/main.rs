a57e468c35f964c988972db468b4fcc2b0734760
Hamid <7atashfeshan@gamil.com>
2020-06-23 15:47:00 +0430

    Add forward header + Implement ChannelInfo

--- src/main.rs
+++ src/main.rs
@@ -89,12 +89,17 @@ async fn run2( mut c: Client){
     // let mut app = Arc::new(Mutex::new(app));
     // let app1 = app.get_mut().unwrap();
 
+    // app.get_dialogs().await;
     app.get_messages().await;
+    app.get_channel_info().await;
 
 }
 
 // types
 use std::collections::HashMap;
+use std::borrow::Borrow;
+use grammers_tl_types::Serializable;
+
 pub struct App {
     login: Vec<LoginPhone>,
     channels: HashMap<i64,ChannelSpace>,
@@ -111,11 +116,20 @@ pub struct MsgReplayTo {
 
 #[derive(Clone, Debug)]
 pub struct MsgForwarded {
-
+    pub from_id: i32,
+    pub from_name: String,
+    pub date: i32,
+    pub channel_id: i32,
+    pub channel_post: i32,
+    pub post_author: String,
+    // pub saved_from_peer: Option<crate::enums::Peer>,
+    pub saved_from_msg_id: i32,
 }
 
 #[derive(Clone, Debug)]
 pub struct Msg {
+    pub silent: bool,
+    pub post: bool,
     pub id: i32,
     pub from_id: i32,
     // pub to_id: crate::enums::Peer,
@@ -132,6 +146,8 @@ pub struct Msg {
     // pub post_author: Option<String>,
     // pub grouped_id: Option<i64>,
     pub restricted: bool,
+    pub forward: Option<MsgForwarded>,
+    pub replay: Option<MsgReplayTo>,
     // raw: tl::types::Message,
 }
 
@@ -152,9 +168,22 @@ pub struct ChannelSpace {
 
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Default, Debug)]
 pub struct ChannelInfo {
-
+    id: i32,
+    title: String,
+    username: String,
+    about: String,
+    link: String,
+    members_count: i32,
+    read_inbox_max_id: i32,
+    access_hash: i64,
+    date: i32,
+    photo: u8,
+    version: i32,
+    pts: i32,
+    restricted: bool,
+    megagroup: bool,
 }
 
 #[derive(Clone, Debug)]
@@ -196,23 +225,87 @@ impl App {
             hash: 0,
         };
         let mt : tl::enums::messages::Dialogs = self.client.invoke(&request).await.unwrap();
-        // println!("dilagos {:#?}", mt);
+        println!("dilagos {:#?}", mt);
+    }
+
+    pub async fn get_channel_info(&mut self) {
+        let request = tl::functions::channels::GetFullChannel {
+            channel: tl::enums::InputChannel::Channel(
+                tl::types::InputChannel{
+                    channel_id: 1072723547,
+                    access_hash: -1615658883512673699,
+                }
+            )
+        };
+        // let res: tl::enums::ChatFull = self.client.invoke(&request).await.unwrap();
+        let res = self.client.invoke(&request).await.unwrap();
+        println!("request {:#?}", res);
+
+        let mut ci = ChannelInfo::default();
+
+        use tl::enums::messages::ChatFull;
+        match res {
+            ChatFull::Full(full)=> {
+
+                use tl::enums::ChatFull;
+                match full.full_chat {
+                   ChatFull::ChannelFull(c) => {
+                       ci.id = c.id;
+                       ci.pts = c.pts;
+                       ci.read_inbox_max_id = c.read_inbox_max_id;
+                       // ci.pts = c.p
+
+                   }
+                    _ => {}
+                }
+
+                // use tl::enums::ChatFull;
+
+                if full.chats.len() == 1{
+                    let chat = full.chats.get(0).unwrap();
+
+                    use tl::enums::Chat;
+                    match chat {
+                        Chat::Channel(ch) => {
+                            ci.id = ch.id;
+                            ci.title = ch.title.clone();
+                            ci.username = ch.username.borrow().as_ref().unwrap_or(&"".to_string()).clone();
+                            ci.access_hash = ch.access_hash.unwrap_or(0);
+                            ci.date =  ch.date;
+                            ci.version =  ch.version;
+                            ci.members_count = ch.participants_count.unwrap_or(0);
+                            ci.megagroup = ch.megagroup;
+                            ci.restricted = ch.restricted;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        println!("channel info {:#?}", ci);
+
     }
 
     pub async fn get_messages(&mut self) {
         let request = tl::functions::messages::GetHistory {
             /*peer: tl::enums::InputPeer::Channel(tl::types::InputPeerChannel{
-                channel_id: 1072723547,
+                channel_id: 1072723547, // telegraph
                 access_hash: -1615658883512673699,
             }),*/
+            /*
             peer: tl::enums::InputPeer::Channel(tl::types::InputPeerChannel{
-                channel_id: 1355843251,
+                channel_id: 1355843251, // codal365
                 access_hash: -6028453276089081451,
+            }),*/
+            peer: tl::enums::InputPeer::Channel(tl::types::InputPeerChannel{
+                channel_id: 1163672339, // forever54321
+                access_hash: -3665401744061121093,
             }),
             offset_id: 0,
             offset_date: 0,
             add_offset: 0,
-            limit: 2,
+            limit: 100,
             max_id: 0,
             min_id: 0,
             hash: 0
@@ -275,32 +368,53 @@ fn process_msgs(mt: tl::enums::messages::Messages) {
                 match m {
                     Message::Message(m2) => {
 
-                        println!(">>> \n {:#?}", m2);
+                       // println!(">>> \n {:#?}", m2);
                         if let Some(f) = m2.media.clone() {
                             // println!("{:?}", f)
-                            
+
                         }
 
                         let ms = message_to_msg(m2.clone());
-                        let mut u =  extract_urls_from_message_text_meta(m2.entities);
+                        let mut u =  extract_urls_from_message_entity(m2.entities);
                         urls.append(&mut u);
                         msgs.push(ms);
                     },
                     _ => {}
                 }
-
             }
         },
         _ => {
             println!("other form of messages!")
         }
     }
-    println!("msgs {:#?} ", msgs);
-    println!("urls {:#?} ", urls);
+    // println!("msgs {:#?} ", msgs);
+    // println!("urls {:#?} ", urls);
 }
 
 fn message_to_msg(m: tl::types::Message) -> Msg{
+    let mut fwd = None ;
+    if let Some(fw) = m.fwd_from {
+        use tl::enums::MessageFwdHeader::*;
+        match fw {
+            Header (f) => {
+                fwd = Some(MsgForwarded {
+                    from_id: f.from_id.unwrap_or(0),
+                    from_name: f.from_name.unwrap_or("".to_string()),
+                    date: f.date,
+                    channel_id: f.channel_id.unwrap_or(0),
+                    channel_post: f.channel_post.unwrap_or(0),
+                    post_author: f.post_author.unwrap_or("".to_string()),
+                    saved_from_msg_id: f.saved_from_msg_id.unwrap_or(0)
+                });
+            }
+
+
+        }
+    };
+    println!("forward {:#?} ", fwd);
     Msg {
+        silent: m.silent,
+        post: m.post,
         id: m.id,
         from_id: m.id,
         via_bot_id: m.via_bot_id.unwrap_or(0),
@@ -310,14 +424,15 @@ fn message_to_msg(m: tl::types::Message) -> Msg{
         views: m.views.unwrap_or(0),
         edit_date: m.edit_date.unwrap_or(0),
         restricted: m.restriction_reason.is_some(),
-        // raw: m,
+        forward: fwd,
+        replay: None
     }
 }
 
-fn extract_urls_from_message_text_meta(m: Option<Vec<tl::enums::MessageEntity>>) -> Vec<String>{
+fn extract_urls_from_message_entity(entities: Option<Vec<tl::enums::MessageEntity>>) -> Vec<String>{
     let mut urls = vec![];
-    if let Some(enti) =  m {
-        for v in enti{
+    if let Some(ent) = entities {
+        for v in ent {
             use tl::enums::MessageEntity::*;
             match v {
                 TextUrl(t)=> {
@@ -326,12 +441,9 @@ fn extract_urls_from_message_text_meta(m: Option<Vec<tl::enums::MessageEntity>>)
                 _ => {},
             }
         }
-
     };
     urls
 }
-
-
 
 /////////////////////////////////////////
 async fn run( mut c: Client){
